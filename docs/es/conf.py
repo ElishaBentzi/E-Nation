48024278082be459b5d5112dc30ee5f03336392d
Elisha Bentzi <elishabentzi@yahoo.com>
2024-05-04 21:20:10 +0200

    Update conf.py

--- docs/es/conf.py
+++ docs/es/conf.py
@@ -58,7 +58,7 @@ master_doc = 'index'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'es'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -155,8 +155,3 @@ texinfo_documents = [
      author, 'E-Nation', 'One line description of project.',
      'Miscellaneous'),
 ]
-
-# Added custom css
-
-def setup(app):
-    app.add_stylesheet('css/custom.css')
